5594e922fa9f80d57c30fd775d89f06baa1feff4
yugendhar1997 <58684163+yugendhar1997@users.noreply.github.com>
2025-07-12 03:36:55 +0530

    Update EditProfile.js

--- client/src/components/EditProfile.js
+++ client/src/components/EditProfile.js
@@ -1,114 +1,51 @@
-import { useEffect, useRef, useState } from "react";
-import { Link } from "react-router-dom";
-import TopNavigation from "./TopNavigation";
 import { useSelector } from "react-redux";
+import TopNavigation from "./TopNavigation";
 
-function EditProfile() {
-  let firstNameInputRef = useRef();
-  let lastNameInputRef = useRef();
-  let ageInputRef = useRef();
-  let emailInputRef = useRef();
-  let passwordInputRef = useRef();
-  let mobileNumberInputRef = useRef();
-  let profilePicInputRef = useRef();
-
-  let [profilePic, SetProfilePic] = useState(
-    "https://pulse.brninfotech.com/media/auth/images/no-pic3.png"
-  );
-
+function Dashboard() {
   let userDetails = useSelector((store) => {
     return store.userDetails;
   });
 
-  useEffect(() => {
-    firstNameInputRef.current.value = userDetails.firstName;
-    lastNameInputRef.current.value = userDetails.lastName;
-    ageInputRef.current.value = userDetails.age;
-    emailInputRef.current.value = userDetails.email;
-    mobileNumberInputRef.current.value = userDetails.mobileNumber;
-    SetProfilePic(`:/${userDetails.profilePic}`);
-  }, []);
-
-  let editProfile = async () => {
-    let dataTosend = new FormData();
-    dataTosend.append("firstName", firstNameInputRef.current.value);
-    dataTosend.append("lastName", lastNameInputRef.current.value);
-    dataTosend.append("age", ageInputRef.current.value);
-    dataTosend.append("email", emailInputRef.current.value);
-    dataTosend.append("password", passwordInputRef.current.value);
-    dataTosend.append("mobileNumber", mobileNumberInputRef.current.value);
-    for (let i = 0; i <= profilePicInputRef.current.files.length; i++) {
-      dataTosend.append("profilePic", profilePicInputRef.current.files[i]);
-    }
+  let onDeleteProfile = async () => {
+    let dataToSend = new FormData();
+    dataToSend.append("email", userDetails.email);
 
     let reqOptions = {
-      method: "PATCH",
-      body: dataTosend,
+      method: "DELETE",
+      body: dataToSend,
     };
 
-    let JSONData = await fetch(":/editProfile", reqOptions);
+    let JSONData = await fetch(
+      ":4444/deleteProfile",
+      reqOptions
+    );
+
     let JSOData = await JSONData.json();
     console.log(JSOData);
     alert(JSOData.msg);
   };
-
   return (
-    <div className="App">
+    <div>
       <TopNavigation></TopNavigation>
-      <h1>Edit Profile</h1>
-      <form>
-        <div>
-          <label>First Name</label>
-          <input type="text" ref={firstNameInputRef}></input>
-        </div>
-        <div>
-          <label>Last Name</label>
-          <input type="text" ref={lastNameInputRef}></input>
-        </div>
-        <div>
-          <label>Age</label>
-          <input type="number" ref={ageInputRef}></input>
-        </div>
-        <div>
-          <label>Email</label>
-          <input type="email" ref={emailInputRef} readOnly></input>
-        </div>
-        <div>
-          <label>Password</label>
-          <input ref={passwordInputRef}></input>
-        </div>
-        <div>
-          <label>Mobile Number</label>
-          <input type="number" ref={mobileNumberInputRef}></input>
-        </div>
-        <div>
-          <label>Profile Pic</label>
-          <input
-            type="file"
-            // multiple
-            ref={profilePicInputRef}
-            onChange={(eo) => {
-              let selectedPath = URL.createObjectURL(eo.target.files[0]);
-              SetProfilePic(selectedPath);
-            }}
-          ></input>
-        </div>
-        <div>
-          <img className="profilePic" src={profilePic} alt=""></img>
-        </div>
-        <div>
-          <button
-            type="button"
-            onClick={() => {
-              editProfile();
-            }}
-          >
-            Edit Profile
-          </button>
-        </div>
-      </form>
+      <h1>Dashboard</h1>
+      <button
+        type="button"
+        onClick={() => {
+          onDeleteProfile();
+        }}
+      >
+        Delete Account
+      </button>
+      <h1>
+        {userDetails.firstName}
+        {userDetails.lastName}
+        <img
+          src={`:4444/${userDetails.profilePic}`}
+          alt=""
+        ></img>
+      </h1>
     </div>
   );
 }
 
-export default EditProfile;
+export default Dashboard;
